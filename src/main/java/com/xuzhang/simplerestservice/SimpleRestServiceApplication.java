8328cbb3b1a9bfab4a8e1c2c5954334c8beae20a
robertxuzh <robert.xu@kronos.com>
2020-03-21 18:17:18 +0800

    first commit

---
+++ src/main/java/com/xuzhang/simplerestservice/SimpleRestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.xuzhang.simplerestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleRestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleRestServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xuzhang/simplerestservice/SimpleRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xuzhang.simplerestservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleRestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
